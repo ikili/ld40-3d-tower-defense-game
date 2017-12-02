518d4d54dd44c978470c23d09cd924b2637688b3
Jonasz Kądziela <jonaszkadziela@o2.pl>
2017-12-02 18:49:46 +0100

    Added shooting mechanics and basic particles

---
+++ To the glory of the great North Korea/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public float speed = 70f;
+	public GameObject bulletImpactParticle;
+
+	private Transform target;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		if (target == null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		Vector3 dir = target.position - transform.position;
+		float distanceThisFrame = speed * Time.deltaTime;
+
+		if (dir.magnitude <= distanceThisFrame)
+		{
+			HitTarget();
+			return;
+		}
+		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+	}
+
+	void HitTarget()
+	{
+		GameObject impactParticle = Instantiate(bulletImpactParticle, transform.position, transform.rotation);
+		Destroy(impactParticle, 2f);
+		Destroy(gameObject);
+		return;
+	}
+
+	public void Seek(Transform _target)
+	{
+		target = _target;
+	}
+}
--- To the glory of the great North Korea/Assets/Scripts/Turret.cs
+++ To the glory of the great North Korea/Assets/Scripts/Turret.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+	[Header("Attributes")]
 	public float range = 15f;
 	public float turnSpeed = 10f;
+	public float fireRate = 1f;
+	[Header("Setup Fields")]
 	public string enemyTag = "Enemy";
 	public Transform partToRotate;
+	public Transform[] firePoint;
+	public GameObject bulletPrefab;
 
 	private Transform target;
+	private float fireCountdown = 0f;
+	private int firePointNum = 0;
 
 	void Start()
 	{
@@ -51,6 +58,30 @@ public class Turret : MonoBehaviour
 		Quaternion lookRotation = Quaternion.LookRotation(dir);
 		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+		if (fireCountdown <= 0)
+		{
+			Shoot();
+			fireCountdown = 1f / fireRate;
+		}
+
+		fireCountdown -= Time.deltaTime;
+	}
+
+	void Shoot()
+	{
+		
+		if (firePointNum > firePoint.Length - 1)
+		{
+			firePointNum = 0;
+		}
+		GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint[firePointNum].position, firePoint[firePointNum].rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet>();
+		firePointNum++;
+		if (bullet != null)
+		{
+			bullet.Seek(target);
+		}
 	}
 
 	void OnDrawGizmosSelected()
