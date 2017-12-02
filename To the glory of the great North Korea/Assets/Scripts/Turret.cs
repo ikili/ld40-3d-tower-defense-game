982a474af0ff59e50b18d34b5af8a351919c217e
Jonasz Kądziela <jonaszkadziela@o2.pl>
2017-12-02 17:24:41 +0100

    Added Turrets

---
+++ To the glory of the great North Korea/Assets/Scripts/Turret.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+	public float range = 15f;
+	public float turnSpeed = 10f;
+	public string enemyTag = "Enemy";
+	public Transform partToRotate;
+
+	private Transform target;
+
+	void Start()
+	{
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+	void UpdateTarget()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject nearestEnemy = null;
+		float shortestDistance = Mathf.Infinity;
+
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+
+		if (nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+		}
+		else
+		{
+			target = null;
+		}
+	}
+
+	void Update()
+	{
+		if (target == null)
+		{
+			return;
+		}
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+}
