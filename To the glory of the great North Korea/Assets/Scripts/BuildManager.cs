482b7458fc2c30b8e36b7df6d7a3332d2eefb462
Jonasz Kądziela <jonaszkadziela@o2.pl>
2017-12-02 19:31:20 +0100

    Added basic build functionality

---
+++ To the glory of the great North Korea/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+	public static BuildManager instance;
+	public GameObject oneGunTurretPrefab;
+
+	private GameObject turretToBuild;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogError("More than one BuildManager in scene!");
+		}
+		instance = this;
+	}
+
+	void Start()
+	{
+		turretToBuild = oneGunTurretPrefab;
+	}
+
+	public GameObject GetTurretToBuild()
+	{
+		return turretToBuild;
+	}
+}
---
+++ To the glory of the great North Korea/Assets/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+	public Color hoverColor;
+
+	private Renderer r;
+	private Color startColor;
+	private GameObject turret;
+
+	void Start()
+	{
+		r = GetComponent<Renderer>();
+		startColor = r.material.color;
+	}
+
+	void OnMouseDown()
+	{
+		if (turret != null)
+		{
+			Debug.Log("Can't build there.");
+			return;
+		}
+
+		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+	}
+
+	void OnMouseEnter()
+	{
+		r.material.color = hoverColor;
+	}
+
+	void OnMouseExit()
+	{
+		r.material.color = startColor;
+	}
+}
