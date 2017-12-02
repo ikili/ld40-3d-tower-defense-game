074dbaf25450b7a69ab654dca2c59ad122c89fce
Jonasz Kądziela <jonaszkadziela@o2.pl>
2017-12-02 10:25:42 +0100

    Added WaveSpawner

---
+++ To the glory of the great North Korea/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+	public Transform enemyPrefab;
+	public Transform[] spawnPoints;
+	public float timeBetweenWaves = 5f;
+
+	private float countdown = 2f;
+	private int waveIndex = 0;
+
+	void Update()
+	{
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+		countdown -= Time.deltaTime;
+	}
+
+	IEnumerator SpawnWave()
+	{
+		waveIndex++;
+		int random;
+		Debug.Log("Wave incomming!");
+		for (int i = 0; i < waveIndex; i++)
+		{
+			random = Random.Range(0, spawnPoints.Length);
+			SpawnEnemy(random);
+			yield return new WaitForSeconds(Random.Range(0.0f, 1.0f));
+		}
+	}
+
+	void SpawnEnemy(int random)
+	{
+		Instantiate(enemyPrefab, spawnPoints[random].position, spawnPoints[random].rotation);
+	}
+}
