cfe165e2d54fb7bb8d1342d8dc7e8630f0d2f61a
Jonasz Kądziela <jonaszkadziela@o2.pl>
2017-12-03 18:32:08 +0100

    Added laser turret and optimized some scripts

--- To the glory of the great North Korea/Assets/Scripts/Enemy.cs
+++ To the glory of the great North Korea/Assets/Scripts/Enemy.cs
@@ -2,31 +2,19 @@
 
 public class Enemy : MonoBehaviour
 {
-	public float speed = 5f;
-	public int health = 100;
+	public float startSpeed = 5f;
+	[HideInInspector]
+	public float speed;
+	public float health = 100f;
 	public int moneyDrop = 50;
 	public GameObject enemyDeathParticle;
 
-	private Transform target;
-	private int wavepointIndex = 0;
-
 	void Start()
 	{
-		target = Waypoints.points[0];
+		speed = startSpeed;
 	}
 
-	void Update()
-	{
-		Vector3 dir = target.position - transform.position;
-		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-		if (Vector3.Distance(transform.position, target.position) <= 0.2f)
-		{
-			GetNextWaypoint();
-		}
-	}
-
-	public void TakeDamage(int amount)
+	public void TakeDamage(float amount)
 	{
 		health -= amount;
 		if (health <= 0)
@@ -35,28 +23,16 @@ public class Enemy : MonoBehaviour
 		}
 	}
 
+	public void Slow(float slowAmount)
+	{
+		speed = startSpeed * (1f - slowAmount);
+	}
+
 	void Die()
 	{
 		PlayerStats.Money += moneyDrop;
 
 		Instantiate(enemyDeathParticle, transform.position, Quaternion.identity);
-		Destroy(gameObject);
-	}
-
-	void GetNextWaypoint()
-	{
-		if (wavepointIndex >= Waypoints.points.Length - 1)
-		{
-			EndPath();
-			return;
-		}
-		wavepointIndex++;
-		target = Waypoints.points[wavepointIndex];
-	}
-
-	void EndPath()
-	{
-		PlayerStats.Lives--;
 		Destroy(gameObject);
 	}
 }
--- To the glory of the great North Korea/Assets/Scripts/Turret.cs
+++ To the glory of the great North Korea/Assets/Scripts/Turret.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-	[Header("Attributes")]
+	[Header("General")]
 	public float range = 15f;
+
+	[Header("Bullets")]
+	public GameObject bulletPrefab;
 	public float turnSpeed = 10f;
 	public float fireRate = 1f;
+
+	[Header("Laser")]
+	public LineRenderer lineRenderer;
+	public ParticleSystem impactParticle;
+	public Light impactLight;
+	public float damageOverTime = 30f;
+	public float slowAmount = 0.5f;
+	public bool useLaser = false;
+
 	[Header("Setup Fields")]
 	public string enemyTag = "Enemy";
 	public Transform partToRotate;
 	public Transform[] firePoint;
-	public GameObject bulletPrefab;
 
 	private Transform target;
+	private Enemy targetEnemy;
 	private float fireCountdown = 0f;
 	private int firePointNum = 0;
 
@@ -41,6 +53,7 @@ public class Turret : MonoBehaviour
 		if (nearestEnemy != null && shortestDistance <= range)
 		{
 			target = nearestEnemy.transform;
+			targetEnemy = nearestEnemy.GetComponent<Enemy>();
 		}
 		else
 		{
@@ -52,25 +65,66 @@ public class Turret : MonoBehaviour
 	{
 		if (target == null)
 		{
+			if (useLaser)
+			{
+				if (lineRenderer.enabled)
+				{
+					lineRenderer.enabled = false;
+					impactParticle.Stop();
+					impactLight.enabled = false;
+				}
+			}
 			return;
 		}
+
+		LockOnTarget();
+
+		if (useLaser)
+		{
+			Laser();
+		}
+		else
+		{
+			if (fireCountdown <= 0)
+			{
+				Shoot();
+				fireCountdown = 1f / fireRate;
+			}
+
+			fireCountdown -= Time.deltaTime;
+		}
+	}
+
+	void LockOnTarget()
+	{
 		Vector3 dir = target.position - transform.position;
 		Quaternion lookRotation = Quaternion.LookRotation(dir);
 		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
 
-		if (fireCountdown <= 0)
+	void Laser()
+	{
+		targetEnemy.TakeDamage(damageOverTime * Time.deltaTime);
+		targetEnemy.Slow(slowAmount);
+
+		if (!lineRenderer.enabled)
 		{
-			Shoot();
-			fireCountdown = 1f / fireRate;
+			lineRenderer.enabled = true;
+			impactLight.enabled = true;
+			impactParticle.Play();
 		}
+		lineRenderer.SetPosition(0, firePoint[0].position);
+		lineRenderer.SetPosition(1, target.position);
 
-		fireCountdown -= Time.deltaTime;
+		Vector3 dir = firePoint[0].position - target.position;
+
+		impactParticle.transform.position = target.position + dir.normalized * 0.2f;
+		impactParticle.transform.rotation = Quaternion.LookRotation(dir);
 	}
 
 	void Shoot()
 	{
-		
 		if (firePointNum > firePoint.Length - 1)
 		{
 			firePointNum = 0;
